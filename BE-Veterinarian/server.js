7197d60fef649998bcf6b39355a26235dc4e2691
JayceNgo <60013030+JayceNgo@users.noreply.github.com>
2025-04-07 00:20:04 -0400

    Fix backend

--- BE-Veterinarian/server.js
+++ BE-Veterinarian/server.js
@@ -1,5 +1,10 @@
 import { app, env } from "./config/index.js";
 
-const PORT = env.PORT || 4000;
+// For local development
+if (process.env.NODE_ENV !== "production") {
+    const PORT = env.PORT || 4000;
+    app.listen(PORT, () => console.log(`Server running at ${PORT}`));
+}
 
-app.listen(PORT, () => console.log(`server running at ${PORT}`));
+// Export the Express app for Vercel
+export default app;
